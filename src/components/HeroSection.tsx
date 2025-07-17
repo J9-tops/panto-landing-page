b6afdf33452a562c5188a9184e63717405d2195d
J9-tops <j9.tops@gmail.com>
2025-07-17 17:04:15 +0100

    feat: add navbar

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,13 @@
+export default function HeroSection() {
+  return (
+    <section className="min-h-screen w-full flex-1">
+      <div className="absolute -top-10 w-full">
+        <img
+          src="https://ik.imagekit.io/tWbKxVjQnHeuMza/landing-pages/Rectangle%201441-min.png?updatedAt=1752760418184"
+          alt=""
+          className="pointer-events-none h-full w-full cursor-none object-cover"
+        />
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+export default function Navbar() {
+  return (
+    <nav className="font-ibm-plex absolute top-0 z-10 container bg-transparent text-yellow-500">
+      <span className="">Panto</span>
+      <ul>
+        <li>
+          <span>Furniture</span>
+        </li>
+        <li>
+          <span>Shop</span>
+        </li>
+        <li>
+          <span>About Us</span>
+        </li>
+        <li>
+          <span>Contact</span>
+        </li>
+      </ul>
+    </nav>
+  );
+}
